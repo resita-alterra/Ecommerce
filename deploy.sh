b63661d468565948612d296b3ea669178803fc8c
resita-alterra <resita@alterra.id>
2019-08-28 14:37:27 +0700

    dummyss

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-docker stop helloworld
-docker rm helloworld
-docker rmi resitasriw/helloworld
-docker run -d --name helloworld -p 3000:80 resitasriw/helloworld:latest
+sudo docker stop helloworld
+sudo docker rm helloworld
+sudo docker rmi resitasriw/helloworld
+sudo docker run -d --name helloworld -p 3000:80 resitasriw/sudo helloworld:latest
