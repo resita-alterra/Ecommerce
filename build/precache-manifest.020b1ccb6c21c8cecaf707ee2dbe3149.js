fee116253ffb74cf2ac96bd6004d790349da353f
resita-alterra <resita@alterra.id>
2019-08-28 14:15:10 +0700

    travis

---
+++ build/precache-manifest.020b1ccb6c21c8cecaf707ee2dbe3149.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "345e58b44090ad852c4e65e2de03f33d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8893bcf3cc4e1e30a8b1",
+    "url": "/static/css/main.5fb0cbae.chunk.css"
+  },
+  {
+    "revision": "724be76e32ba64d89130",
+    "url": "/static/js/2.c53aabd8.chunk.js"
+  },
+  {
+    "revision": "8893bcf3cc4e1e30a8b1",
+    "url": "/static/js/main.2e378a49.chunk.js"
+  },
+  {
+    "revision": "216a33ac354e25f75e34",
+    "url": "/static/js/runtime~main.f0a0950f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,4 @@
-eval "$(ssh-agent -s)" &&
-ssh-add -k ~/.ssh/id_rsa &&
-cd /var/www/helloworld
-git pull
-
-source ~/.profile
-echo "$DOCKERHUB_PASS" | docker login --username $DOCKERHUB_USER --password-stdin
 docker stop helloworld
 docker rm helloworld
-docker rmi aprynur/helloworld
-docker run -d --name helloworld -p 2000:2000 resitasriw/helloworld:latest
+docker rmi resitasriw/helloworld
+docker run -d --name helloworld -p 3000:80 resitasriw/helloworld:latest
